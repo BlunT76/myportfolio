df2fb84222a933d627c556e48e8dbf2d5f8db679
bob <huhmiel@free.Fr>
2019-06-05 17:29:44 +0200

    Updates

---
+++ precache-manifest.6b482001f50c33a535766c189b1bfe7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fdeac79164da88c66581f2e46bb90a0",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "c5badef23bb828c175e1",
+    "url": "/myportfolio/static/css/2.d9a3577d.chunk.css"
+  },
+  {
+    "revision": "41d9d1422d6c7a799e48",
+    "url": "/myportfolio/static/css/main.4e5ca6cb.chunk.css"
+  },
+  {
+    "revision": "c5badef23bb828c175e1",
+    "url": "/myportfolio/static/js/2.2a5d1659.chunk.js"
+  },
+  {
+    "revision": "41d9d1422d6c7a799e48",
+    "url": "/myportfolio/static/js/main.2efc4c55.chunk.js"
+  },
+  {
+    "revision": "314136164c1c0c643422",
+    "url": "/myportfolio/static/js/runtime~main.69c47bdd.js"
+  }
+]);
